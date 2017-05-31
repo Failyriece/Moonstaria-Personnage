7e114c16ca6e6abd5e396e4193ef86cc6403cf5d
Failyriece <failyriece@gmail.com>
2017-05-31 10:18:45 +0200

    Ajout d'une image du ReadMe et des premiers test de page principale

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Moonstaria</title>
+		<link rel="stylesheet" href="css/css.css">
+	</head>
+	<body>
+		
+		<form action="#" method="post">
+			<label for="Groupe" class="block">A la recherche d'un groupe?</label>
+			<input type="text" name="Groupe" id="Groupe" placeholder="Groupe?" class="block">
+			<input type="button" value="Afficher" class="block">
+		</form>
+		<form action="#" method="post">
+			<label for="Joueur" class="block">A la recherche d'un joueur?</label>
+			<input type="text" name="Joueur" id="Joueur" placeholder="Joueur?" class="block">
+			<input type="button" value="Modifier">
+			<input type="button" value="Afficher">
+		</form>
+		<script src="js/jquery-3.1.1.min.js"></script>
+		<script src="js/turn.min.js"></script>
+		<script src="js/main.js"></script>
+	</body>
+</html>
